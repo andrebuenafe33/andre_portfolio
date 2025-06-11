013978de08c1ec2d9f4dd00797cb14e04aa66f4e
andrebuenafe33 <andrejosephbuenafe231@gmail.com>
2025-06-11 18:08:04 +0800

    fixing some features

--- src/components/Achievements/achievement.js
+++ src/components/Achievements/achievement.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './achievement.css';
 import Achievement1 from '../../assets/achievement.png';
 import Achievement2 from '../../assets/achievement.png';
@@ -7,8 +7,30 @@ import Achievement4 from '../../assets/achievement.png';
 import FacebookIcon from '../../assets/facebook.jpg';
 import InstagramIcon from '../../assets/instagram.jpg';
 import GitHubIcon from '../../assets/github.png';
+import emailjs from '@emailjs/browser';
 
 const Achievements = () => {
+  // EmailJS configuration
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_gyqabll', 'template_ms0e8qh', form.current, {
+        publicKey: 'oBpJvLXt5ydgEKkix',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset(); // Reset the form after successful submission
+          alert('Message sent successfully!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+  
   return (
     <section id="contactPage">
       <div id="achievements">
@@ -28,10 +50,10 @@ const Achievements = () => {
         <span className="contactDescription">
           I am always open to new opportunities, collaborations, or professional connections. If you would like to discuss a project, have any questions, or simply want to connect, please feel free to reach out. I look forward to hearing from you.
         </span>
-        <form className="contactForm">
-          <input type="text" className="name" placeholder="Your Name" />
-          <input type="email" className="email" placeholder="Your Email" />
-          <textarea className="message" placeholder="Your Message" rows="13"></textarea>
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+          <input type="text" name="your_name" className="name" placeholder="Your Name" />
+          <input type="email" name="your_email" className="email" placeholder="Your Email" />
+          <textarea name="message" className="message" placeholder="Your Message" rows="13"></textarea>
           <button type="submit" value="Send" className="submitBtn">Send Message</button>
           <div className="Links">
             <img src={FacebookIcon} className="icon1" alt="Facebook" />
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/ajb-logo2.png';
 import { Link } from 'react-scroll';
 import contactMeImg from '../../assets/contactMe.jpg';
+import burgerMenu from '../../assets/burgerMenu.png';
 
 const Navbar = () => {
+
+  // Burger Menu Toogle Functionality
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
         <img src={logo} alt="Logo" className="logo" />
         <div className="desktopMenu">
-            <Link className="desktopMenuListItem">Home</Link>
-            <Link className="desktopMenuListItem">About</Link>
-            <Link className="desktopMenuListItem">Portfolio</Link>
-            <Link className="desktopMenuListItem">Achievements</Link>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} title="Home" className="desktopMenuListItem">Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-80} duration={500} title="About" className="desktopMenuListItem">About</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-80} duration={500} title="Portfolio" className="desktopMenuListItem">Portfolio</Link>
+            <Link activeClass='active' to='achievements' spy={true} smooth={true} offset={-80} duration={500} title="Achievements" className="desktopMenuListItem">Achievements</Link>
         </div>
-        <button className="desktopMenuBtn">
+        <button className="desktopMenuBtn" onClick={() => {
+          document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+        }}>
             <img src={contactMeImg} alt="Contact Me" className="desktopMenuImg" />Contact Me
         </button>
+        
+        {/* Burger Menu */}
+        <img src={burgerMenu} alt="Menu" className="mobileMenu" onClick={() => setShowMenu(!showMenu)}/>
+        <div className="navMenu" style={{display: showMenu? 'flex' : 'none'}}>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} title="Home" className="listItem" onClick={() => setShowMenu(false)}>Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-80} duration={500} title="About" className="listItem" onClick={() => setShowMenu(!showMenu)}>About</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-80} duration={500} title="Portfolio" className="listItem" onClick={() => setShowMenu(!showMenu)}>Portfolio</Link>
+            <Link activeClass='active' to='achievements' spy={true} smooth={true} offset={-80} duration={500} title="Achievements" className="listItem" onClick={() => setShowMenu(!showMenu)}>Achievements</Link>
+            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-70} duration={500} title="Contact" className="listItem" onClick={() => setShowMenu(!showMenu)}>Contact</Link>
+        </div>
     </nav>
   )
 }
 
-export default Navbar;
+export default Navbar; 
